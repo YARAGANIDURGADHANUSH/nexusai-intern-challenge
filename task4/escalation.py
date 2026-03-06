f8c0b0992ada4f246af396f5f7c7ff30c155e437
YARAGANI DURGA DHANUSH <ydurgadhanush@gmail.com>
2026-03-06 10:59:51 +0530

    Add escalation logic based on confidence and sentiment

--- task4/escalation.py
+++ task4/escalation.py
@@ -1,1 +1,12 @@
+def should_escalate(context, confidence_score, sentiment_score, intent):
 
+    if confidence_score < 0.65:
+        return True,"low_confidence"
+
+    if sentiment_score < -0.6:
+        return True,"angry_customer"
+
+    if intent == "service_cancellation":
+        return True,"service_cancellation"
+
+    return False,"ai_can_handle"
